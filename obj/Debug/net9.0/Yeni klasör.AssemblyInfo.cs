03456a5f8be30a5745d639e608e0cb182d075d44
berkay umit oksuz <oksuzberkay31mail.com>
2026-05-04 04:56:21 +0300

    tanitim sayfasi eklendi

--- obj/Debug/net9.0/Yeni klasör.AssemblyInfo.cs
+++ obj/Debug/net9.0/Yeni klasör.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yeni klasör")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e095a8d19f24458d8362dcbab1a915594ba8f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yeni klasör")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yeni klasör")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
